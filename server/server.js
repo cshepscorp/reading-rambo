fb1b943639748e2da018ddc1bc2d40c76b4accd4
cshepscorp <sheppard.christy@gmail.com>
2022-02-02 11:31:42 -0500

    heroku deploy

--- server/server.js
+++ server/server.js
@@ -1,21 +1,21 @@
-const express = require('express');
-
+const express = require("express");
+const path = require("path");
 // import ApolloServer
-const { ApolloServer } = require('apollo-server-express');
-const { authMiddleware } = require('./utils/auth');
+const { ApolloServer } = require("apollo-server-express");
+const { authMiddleware } = require("./utils/auth");
 // import our typeDefs and resolvers
-const { typeDefs, resolvers } = require('./schemas');
-const db = require('./config/connection');
+const { typeDefs, resolvers } = require("./schemas");
+const db = require("./config/connection");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 const startServer = async () => {
   // create a new Apollo server and pass in our schema data
-  const server = new ApolloServer({ 
-    typeDefs, 
-    resolvers, 
-    context: authMiddleware, 
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: authMiddleware,
   });
 
   // Start the Apollo server
@@ -34,7 +34,16 @@ startServer();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-db.once('open', () => {
+// Serve up static assets for production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+}
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
+
+db.once("open", () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
   });
