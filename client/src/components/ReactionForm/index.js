3cf2a7e5acbe2e831a5236b89b281b28f4396ac8
cshepscorp <sheppard.christy@gmail.com>
2022-02-09 11:15:15 -0500

    minor changes to reaction form

--- client/src/components/ReactionForm/index.js
+++ client/src/components/ReactionForm/index.js
@@ -1,12 +1,12 @@
-import React, { useState } from 'react';
-import './style.css';
-import { useMutation } from '@apollo/client';
-import { ADD_REACTION } from '../../utils/mutations';
-import { QUERY_SINGLE_MEDIA } from '../../utils/queries';
-import { Button, Container, TextField } from '@mui/material';
+import React, { useState } from "react";
+import "./style.css";
+import { useMutation } from "@apollo/client";
+import { ADD_REACTION } from "../../utils/mutations";
+import { QUERY_SINGLE_MEDIA } from "../../utils/queries";
+import { Button, Container, TextField } from "@mui/material";
 
 const ReactionForm = ({ mediaId }) => {
-  const [reactionBody, setBody] = useState('');
+  const [reactionBody, setBody] = useState("");
   // state change for how many characters are written in the text field
   const [characterCount, setCharacterCount] = useState(0);
   // const [addReaction, { error }] = useMutation(ADD_REACTION);
@@ -17,20 +17,22 @@ const ReactionForm = ({ mediaId }) => {
         // could potentially not exist yet, so wrap in a try/catch
         const { media } = cache.readQuery({
           query: QUERY_SINGLE_MEDIA,
-          variables: { mediaId }
+          variables: { mediaId },
         });
-        // console.log("=====mediaId from mutation");
-        // console.log(mediaId);
-        console.log('=====media from mutation');
+
+        console.log("=====media from mutation");
         console.log(media);
+        const reactions = media.reactions;
+        console.log("=====reactions");
+        console.log(reactions);
         cache.writeQuery({
           query: QUERY_SINGLE_MEDIA,
-          data: { media: [addReaction, ...media] }
+          data: { media: {...media, reactions: [addReaction, ...reactions]} },
         });
       } catch (e) {
         console.error(e);
       }
-    }
+    },
   });
 
   // update state based on form input changes
@@ -42,7 +44,8 @@ const ReactionForm = ({ mediaId }) => {
   };
 
   // submit form
-  const handleFormSubmit = async () => {
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
     try {
       await addReaction({
         variables: { reactionBody, mediaId },
@@ -69,15 +72,15 @@ const ReactionForm = ({ mediaId }) => {
         </p>
         <form onSubmit={handleFormSubmit}>
           <TextField
-            id='reaction-text-field'
+            id="reaction-text-field"
             multiline
             maxRows={6}
-            placeholder='Leave a reaction to this thought...'
+            placeholder="Leave a reaction to this thought..."
             value={reactionBody}
             onChange={handleChange}
           ></TextField>
           <br></br>
-          <Button id='react-submit' type='submit'>
+          <Button id="react-submit" type="submit">
             Submit
           </Button>
         </form>
